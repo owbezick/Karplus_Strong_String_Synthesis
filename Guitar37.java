ca86642a855a16ed599aaa044738c7c1cd5dca0d
Owen Bezick <owbezick@davidson.edu>
2019-10-09 21:21:34 -0400

    initial upload
    Initial upload of files

---
+++ Guitar37.java
@@ -0,0 +1,12 @@
+
+/** A skeleton for the Guitar37 class. Replace this with a better Javadoc! */
+
+public class Guitar37 implements Guitar {
+
+    // keyboard layout
+    public static final String KEYBOARD = 
+        "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    
+    // COMPLETE ME!
+
+}
